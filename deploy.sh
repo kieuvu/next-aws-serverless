f9cfa144550d4098c8e83bad2014f706a8dbd37b
“kieuvu” <kieuvuvuvu@gmail.com>
2023-07-11 15:30:23 +0700

    choose lambda function before deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,29 @@
 
 BUILD_FOLDER=.dist
 
+rm -rf .next
+
+npm run-script build
+
+rm -rf $BUILD_FOLDER
+mv .next/standalone/ $BUILD_FOLDER/
+cp -r .next/static $BUILD_FOLDER/.next
+rm $BUILD_FOLDER/server.js
+cp -r next.config.js $BUILD_FOLDER/
+cp serverless.yaml $BUILD_FOLDER/
+cp -r functions $BUILD_FOLDER/
+cp server.ts $BUILD_FOLDER/
+cp -r public $BUILD_FOLDER/
 cd $BUILD_FOLDER
 
-sls deploy
+echo "Enter function name ('*' to deploy all function):"
+read deploy_function
 
+if [[ "$deploy_function" == "*" ]]; then
+  sls deploy
+else
+  sls deploy function -f "$deploy_function"
+fi
+
+cd ..
 rm -rf $BUILD_FOLDER
