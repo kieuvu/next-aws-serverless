0d5a632e21968d01b0c02b56c558a69f70bbc125
“kieuvu” <kieuvuvuvu@gmail.com>
2023-07-13 16:07:29 +0700

    change assetPrefix config before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
-BUILD_FOLDER=.dist
+DISTRIBUTION_FOLDER=.dist
+NEXT_CONFIG_FILE=next.config.js
 
 rm -rf .next
 
+sed -i 's/\/\/ assetPrefix/assetPrefix/' $NEXT_CONFIG_FILE
+echo "Next Config:"
+cat $NEXT_CONFIG_FILE
+
 npm run-script build
 
-rm -rf $BUILD_FOLDER
-mv .next/standalone/ $BUILD_FOLDER/
-cp -r .next/static $BUILD_FOLDER/.next
-rm $BUILD_FOLDER/server.js
-cp -r next.config.js $BUILD_FOLDER/
-cp serverless.yaml $BUILD_FOLDER/
-cp server.ts $BUILD_FOLDER/
-cp queueWorker.ts $BUILD_FOLDER/
-cp -r public $BUILD_FOLDER/
-cd $BUILD_FOLDER
+rm -rf $DISTRIBUTION_FOLDER
 
-echo "Enter function name ('*' to deploy all function):"
-read deploy_function
+mv .next/standalone/ $DISTRIBUTION_FOLDER/
+cp -r .next/static $DISTRIBUTION_FOLDER/.next
+rm $DISTRIBUTION_FOLDER/server.js
 
-if [[ "$deploy_function" == "*" ]]; then
+cp -r next.config.js $DISTRIBUTION_FOLDER/
+sed -i 's/assetPrefix/\/\/ assetPrefix/' $NEXT_CONFIG_FILE
+
+cp serverless.yaml $DISTRIBUTION_FOLDER/
+cp server.ts $DISTRIBUTION_FOLDER/
+cp queueWorker.ts $DISTRIBUTION_FOLDER/
+cp -r public $DISTRIBUTION_FOLDER/
+
+cd $DISTRIBUTION_FOLDER
+
+echo "Enter function name (Input blank to deploy all functions):"
+read DEPLOY_FUNCTION
+
+if [[ "$DEPLOY_FUNCTION" == "" ]]; then
   sls deploy
 else
-  sls deploy function -f "$deploy_function"
+  sls deploy function -f "$DEPLOY_FUNCTION"
 fi
 
 cd ..
-rm -rf $BUILD_FOLDER
+rm -rf $DISTRIBUTION_FOLDER
